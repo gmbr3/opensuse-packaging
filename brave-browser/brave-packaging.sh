ac3e01fc2afa0109dac89c1abcd4c5033e12842d
Callum Farmer <callumjfarmer13@gmail.com>
2021-01-20 11:04:26 +0000

    update 2

--- brave-browser/brave-packaging.sh
+++ brave-browser/brave-packaging.sh
@@ -7,16 +7,15 @@ rm brave-browser-$old_version_number-patches.tar.xz
 rm brave-browser-$old_version_number.tar.xz
 mkdir brave-browser-$version_number
 cd brave-browser-$version_number
-git clone https://chromium.googlesource.com/chromium/src.git -b $chrome_version_number --depth 1
-cd src
+git clone https://chromium.googlesource.com/chromium/src.git -b $chrome_version_number --depth 1 .
 mkdir brave
 cd brave
 git clone https://github.com/brave/brave-core.git -b v$version_number  --depth 1 .
 mv patches brave-browser-$version_number-patches
 tar cJf brave-browser-$version_number-patches.tar.xz brave-browser-$version_number-patches
-mv brave-browser-$version_number-patches.tar.xz ../../../brave-browser-$version_number-patches.tar.xz
+mv brave-browser-$version_number-patches.tar.xz ../../brave-browser-$version_number-patches.tar.xz
 rm -rf brave-browser-$version_number-patches
-cd ../../..
+cd ../..
 tar cJf brave-browser-$version_number.tar.xz brave-browser-$version_number
 rm -rf brave-browser-$version_number
 osc ar
